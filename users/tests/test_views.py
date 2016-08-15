b80b71458d50cc065529feb174d339bf82212048
forestin <Anikjev@gmail.com>
2016-08-15 16:12:10 +0900

    delete users app

--- users/tests/test_views.py
+++
@@ -1,7 +0,0 @@
-from django.test import TestCase
-
-class HomePageTest(TestCase):
-
-    def test_home_page_renders_home_template(self):
-        response = self.client.get('/')
-        self.assertTemplateUsed(response, 'home.html')
